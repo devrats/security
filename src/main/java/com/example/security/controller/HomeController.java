ec7350d1a213722fb085cd2c32632ec3879dc679
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-05-27 13:33:03 +0530

    updated security configuration

--- src/main/java/com/example/security/controller/HomeController.java
+++ src/main/java/com/example/security/controller/HomeController.java
@@ -8,7 +8,13 @@
 package com.example.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "home";
+    }
 }
