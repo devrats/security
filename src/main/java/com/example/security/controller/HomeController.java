587f29fae7499facd12bd20d1aa707bbec01911a
Sharma Ji <devvrat.sharm_cs19@gla.ac.in>
2021-05-27 13:13:26 +0530

    created basic template with dependencies.

---
+++ src/main/java/com/example/security/controller/HomeController.java
@@ -0,0 +1,14 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devvrat Sharma (devrats)
+ *   Date: 5/27/2021
+ *   Time: 1:12 PM
+ *   File: HomeController.java
+ */
+
+package com.example.security.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
